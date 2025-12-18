fdc9b41e1cd86d1e73b9d5291ef19a458810c139
joshTopp <toppj1247@gmail.com>
2025-12-17 18:18:34 -0600

    Added x, y, width, and height for pose and skateboard. Prints the coords of skateboard and human. Next step add a temporal CNN or LSTM (unsure which one I want)

--- README.md
+++ README.md
@@ -1,27 +1,31 @@
-# Skateboard_Trick_Classifier
-An end-to-end computer vision pipeline for classification of skateboard tricks using OpenCV and YOLOv8. Detecting the skater and skateboard extracting keypoints, and applies temporal model to classify the trick
+# Skateboard Trick Classifier
+An end-to-end computer vision pipeline for classification of skateboard tricks using OpenCV and YOLO11. Using YOLO pose estimation for the skater and YOLO boxing for the skateboard extracting keypoints (I used YOLO because MediaPipe doesn't do well in this situation as I tested and OpenPose seems a little outdated. I was planning using HRNet but there is no point to complicate it as YOLO is really accurate) I plan on starting out using a temporal CNN, but later I will compare with an optimized frozen param ViVit Hugging Face model.
 
 ---
 
 ## Current goals:
-  -Reliable classification of tricks listed below:
+ - Reliable classification of tricks listed below: 
    - Ollie
    - Pop shuvit
    - Kickflip
-  -Build an extensible detection --> pose --> classification pipeline
-  
+ 
+ - Implement a simple Fast API frontend to allow you to submit a trick video
+ - Video → Detection → Pose Estimation → Temporal Modeling → Classify → Deploy
+
 ---
 
 ## Future Goals:
-  -Expand to more tricks that are more complex to classify (e.g., treflips)
-  -Contanizerize using Docker
-  -Deploy using AWS
-  
----
+  - Expand to more tricks that are more complex to classify (e.g., treflips, heelflips, laserflip, 3shuv, body varials, etc.)
+
+  - Containerize using Docker
+
+  - A simple deploy using AWS
+
+-----
 
 ## Dataset
 Training data consists of short video clips extracted from publicly available skateboarding videos(mostly from Youtube) The dataset includes ollie, pop shuvit, and kickflip examples(for now).
 
-The video clips are not distributed by me due to copyght considerations and ethical use. I'm developing this project with respect to privacy and ethical data use. This model solely classifies the trick rather than recognizing people.
+The video clips are not distributed by me due to copyright considerations and ethical use. I'm developing this project with respect to privacy and ethical data use. This model solely classifies the trick rather than recognizing people.
 
 Any sample media that is included, if applicable, will be recorded by the author of the author.
--- main.py
+++ main.py
@@ -1,32 +1,42 @@
 import cv2
 import numpy as np
-from mediapipe.python.solutions.drawing_utils import draw_landmarks
 from ultralytics import YOLO
-import mediapipe as mp
 from transformers import VivitImageProcessor, VivitForVideoClassification
 import torch
 def main():
-
-    model = YOLO("yolo11n.pt")
-    model2 = YOLO("yolo11n-pose.pt")
-    seg_model = YOLO("yolo11n-seg.pt")
+    box_model = YOLO("yolo11n.pt")
+    pose_model = YOLO("yolo11n-pose.pt")
+    #seg_model = YOLO("yolo11n-seg.pt")
 
     cap = cv2.VideoCapture("videos/video.mp4")
     frame_count = 0
     while cap.isOpened():
-        print("hello")
         if frame_count % 2 == 0:
             ret, frame = cap.read()
             if not ret:
                 break
-            seg_results = seg_model(frame, conf=0.5)
-            pose_results = model2(frame, conf=0.5)
-            #draw_skateboard(frame, results)
-            #draw_pose(frame, pose_results)
-            annotated_frame = seg_results[0].plot()
-            annotated_frame2 = pose_results[0].plot()
+            board_results = box_model.predict(frame, conf=0.5, classes=[36])[0]
+            pose_results = pose_model.predict(frame, conf=0.5)[0]
+
+            # FOR TESTING _____________________________________
+            annotated_frame = board_results.plot()
+            annotated_frame2 = pose_results.plot()
             combined_frame = cv2.addWeighted(annotated_frame, 0.5, annotated_frame2, 0.5, 0 )
             cv2.imshow("YOLO Detection", combined_frame)
+            # __________________________________________________
+
+            # this will for boxing to give to the Temporal CNN and ViViT
+            img_height, img_width = frame.shape[:2]
+
+            if len(board_results.boxes ) > 0 and len(pose_results.boxes) > 0:
+                #get the multiplier of the board results multiply it with the height and width print it
+                board_x, board_y, board_width, board_height = board_results.boxes[0].xywhn.flatten()
+                board_x, board_y, board_width, board_height = board_x * img_width, board_y * img_height, board_width * img_width, board_height * img_height
+                print(f"board: {board_x}, {board_y}, {board_width}, {board_height}")
+                # same thing but with the pose results
+                human_x, human_y, human_width, human_height = pose_results.boxes[0].xywhn.flatten()
+                human_x, human_y, human_width, human_height = human_x * img_width, human_y * img_height, human_width * img_width, human_height * img_height
+                print(f"human: {human_x}, {human_y}, {human_width}, {human_height}")
         frame_count += 1
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
