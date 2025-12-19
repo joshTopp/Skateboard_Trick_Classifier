6798e743a1d01095518778f2ef25ed9a4bba6a89
joshTopp <toppj1247@gmail.com>
2025-12-19 16:24:53 -0600

    Resizing works! There should be no bias here because the board will be the same size it won't be stretched or shrunk and adds black padding. I did some research and that should be fine because apparently CNNs tend to ignore it, but if there is bias I will figure something out.

--- main.py
+++ main.py
@@ -32,26 +32,26 @@ def main():
                     pose_results = pose_model.predict(frame, conf=0.5, classes=[0], max_det=1)[0]
 
                     # FOR TESTING _____________________________________
-                    annotated_frame = board_results.plot()
-                    annotated_frame2 = pose_results.plot()
-                    combined_frame = cv2.addWeighted(annotated_frame, 0.5, annotated_frame2, 0.5, 0 )
-                    cv2.imshow("YOLO Detection", combined_frame)
+                    #annotated_frame = board_results.plot()
+                    #annotated_frame2 = pose_results.plot()
+                    #combined_frame = cv2.addWeighted(annotated_frame, 0.5, annotated_frame2, 0.5, 0 )
+                    #cv2.imshow("YOLO Detection", combined_frame)
                     #cv2.imshow("Frame", annotated_frame)
                     #cv2.imshow("Frame", annotated_frame2)
                     # __________________________________________________
 
                     # # this will for boxing to give to the Temporal CNN and ViViT
-                    img_height, img_width = frame.shape[:2]
+                    img_length, img_height = frame.shape[:2]
 
                     if len(board_results.boxes ) > 0:
-                        #get the multiplier of the board results multiply it with the height and width print it
-                        revised_frame = get_boarding_boxes(board_results, img_width, img_height, frame)
+                        #get the multiplier of the board results multiply it with the width and height print it
+                        revised_frame = get_boarding_boxes(board_results, img_height, img_length, frame)
                         #cv2.imshow("Frame", revised_frame)
                         skateboard_frames.append(revised_frame)
 
                     #if len(pose_results.boxes ) > 0:
                         # #same thing but with the pose results
-                        #revised_frame = get_boarding_boxes(pose_results, img_width, img_height, frame)
+                        #revised_frame = get_boarding_boxes(pose_results, img_height, img_length, frame)
                         #human_frames.append(revised_frame)
 
                 frame_count += 1
@@ -64,30 +64,41 @@ def main():
             video_count+=1
 
 
-def get_boarding_boxes(results, img_width, img_height, frame):
+def get_boarding_boxes(results, img_height, img_length, frame):
     box_x, box_y, box_x2, box_y2 = results.boxes.xyxy[0]
     box_x = max(0, int(box_x.item()))
     box_y = max(0, int(box_y.item()))
-    box_x2 = min(img_width, int(box_x2.item()))
-    box_y2 = min(img_height, int(box_y2.item()))
+    box_x2 = min(img_height, int(box_x2.item()))
+    box_y2 = min(img_length, int(box_y2.item()))
     print(f"Boarding box: {box_x}, {box_y}, {box_x2}, {box_y2}")
     # cv2.imshow("Frame", frame[board_y:board_y2, board_x:board_x2])
     return resize_frame(frame[box_y:box_y2, box_x:box_x2], box_x, box_y, box_x2, box_y2)
 
 
 #resizes by 224x224 and adds padding so it doesn't make the board seems expanded or shrunken
-def resize_frame(frame, box_x, box_y, box_x2, box_y2):
-    length = box_x2 - box_x
-    width = box_y2- box_y
-    # im trying to get the image to be 224x224 universally for every frame might change for human change though
-    if length > width:
-        multiplier = 224/length
-    else:
+def resize_frame(box_frame, box_x, box_y, box_x2, box_y2):
+    width = box_x2 - box_x
+    height = box_y2- box_y
+    # im trying to get the image to be 224x224 universally for every frame might change for human resize though
+    if width > height:
         multiplier = 224/width
+    else:
+        multiplier = 224/height
 
-    resized_frame = cv2.resize(frame, (length*multiplier, width*multiplier))
-    resized_frame = cv2.copyMakeBorder(resized_frame, top=int((224-width)/2), bottom=int((224-width)/2),
-                                       right=int((224-length)/2), left=int((224-length)/2), value=(0,0,0),
+    width = int(width * multiplier)
+    height = int(height * multiplier)
+    top = (224 - height) // 2
+    bottom = (224 - height) // 2
+    right = (224 - width) // 2
+    left = (224 - width) // 2
+    if top + bottom + height != 224:
+        bottom+=1
+    if left + right + width != 224:
+        right+=1
+
+    resized_frame = cv2.resize(box_frame, (width, height))
+    resized_frame = cv2.copyMakeBorder(resized_frame, top=top, bottom=bottom,
+                                       right=right, left=left, value=(0,0,0),
                                        borderType=cv2.BORDER_CONSTANT)
     return resized_frame
 
