659b4ef63dbf269c9ab06806ec13348539027f7e
joshTopp <toppj1247@gmail.com>
2025-12-18 19:27:20 -0600

    Changed it so we only focus on the board right now. I think that it is more important for the basic tricks. I will add the pose once I set up the CNN correctly first with only the skateboard. Added the simple directory traverse to get all the training data. Once I finished splicing the videos.

--- main.py
+++ main.py
@@ -1,45 +1,76 @@
+import os
 import cv2
 import numpy as np
 from ultralytics import YOLO
 from transformers import VivitImageProcessor, VivitForVideoClassification
 import torch
+
 def main():
     box_model = YOLO("yolo11n.pt")
     pose_model = YOLO("yolo11n-pose.pt")
     #seg_model = YOLO("yolo11n-seg.pt")
+    video_count = 1
+    dict= {}
+    for video_filename in os.listdir("test"):
+        print(video_filename)
+        dict[video_count]= {}
+        dict[video_count]["skateboard"] = []
+        #dict[video_count]["human"] = []
+        dict[video_count]["label"] = None
+        skateboard_frames = []
+        #human_frames = []
+        for video in os.listdir("test/"+video_filename):
+            cap = cv2.VideoCapture("test/"+video_filename+"/"+video)
+            frame_count = 0
+            while cap.isOpened():
 
-    cap = cv2.VideoCapture("videos/video.mp4")
-    frame_count = 0
-    while cap.isOpened():
-        if frame_count % 4 == 0:
-            ret, frame = cap.read()
-            if not ret:
-                break
-            board_results = box_model.predict(frame, conf=0.5, classes=[36])[0]
-            pose_results = pose_model.predict(frame, conf=0.5)[0]
+                if frame_count % 4 == 0:
+                    ret, frame = cap.read()
+                    if not ret:
+                        break
+                    board_results = box_model.predict(frame, conf=0.3, classes=[36])[0]
+                    pose_results = pose_model.predict(frame, conf=0.5)[0]
 
-            # FOR TESTING _____________________________________
-            annotated_frame = board_results.plot()
-            annotated_frame2 = pose_results.plot()
-            combined_frame = cv2.addWeighted(annotated_frame, 0.5, annotated_frame2, 0.5, 0 )
-            cv2.imshow("YOLO Detection", combined_frame)
-            # __________________________________________________
+                    # FOR TESTING _____________________________________
+                    annotated_frame = board_results.plot()
+                    annotated_frame2 = pose_results.plot()
+                    combined_frame = cv2.addWeighted(annotated_frame, 0.5, annotated_frame2, 0.5, 0 )
+                    cv2.imshow("YOLO Detection", combined_frame)
+                    #cv2.imshow("Frame", annotated_frame)
+                    #cv2.imshow("Frame", annotated_frame2)
+                    # __________________________________________________
 
-            # this will for boxing to give to the Temporal CNN and ViViT
-            img_height, img_width = frame.shape[:2]
+                    # # this will for boxing to give to the Temporal CNN and ViViT
+                    img_height, img_width = frame.shape[:2]
 
-            if len(board_results.boxes ) > 0 and len(pose_results.boxes) > 0:
-                #get the multiplier of the board results multiply it with the height and width print it
-                board_x, board_y, board_width, board_height = board_results.boxes[0].xywhn.flatten()
-                board_x, board_y, board_width, board_height = board_x * img_width, board_y * img_height, board_width * img_width, board_height * img_height
-                print(f"board: {board_x}, {board_y}, {board_width}, {board_height}")
-                # same thing but with the pose results
-                human_x, human_y, human_width, human_height = pose_results.boxes[0].xywhn.flatten()
-                human_x, human_y, human_width, human_height = human_x * img_width, human_y * img_height, human_width * img_width, human_height * img_height
-                print(f"human: {human_x}, {human_y}, {human_width}, {human_height}")
-        frame_count += 1
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+                    if len(board_results.boxes ) > 0:
+                        #get the multiplier of the board results multiply it with the height and width print it
+                        board_x, board_y, board_x2, board_y2 = board_results.boxes.xyxy[0]
+                        board_x = max(0, int(board_x.item()))
+                        board_y = max(0, int(board_y.item()))
+                        board_x2 = min(img_width, int(board_x2.item()))
+                        board_y2 = min(img_height, int(board_y2.item()))
+                        print(f"board: {board_x}, {board_y}, {board_x2}, {board_y2}")
+                        #cv2.imshow("Frame", frame[board_y:board_y2, board_x:board_x2])
+                        skateboard_frames.append(frame[board_y:board_y2, board_x:board_x2])
 
+                    #if len(pose_results.boxes ) > 0:
+                        # #same thing but with the pose results
+                        # human_x, human_y, human_x2, human_y2 = pose_results.boxes.xyxy[0]
+                        # human_x = max(0, int(human_x.item()))
+                        # human_y = max(0, int(human_y.item()))
+                        # human_x2 = min(img_width, int(human_x2.item()))
+                        # human_y2 = min(img_height, int(human_y2.item()))
+                        # print(f"human: {human_x}, {human_y}, {human_x2}, {human_y2}")
+                        # cv2.imshow("Frame", annotated_frame2[human_y:human_y2, human_x:human_x2])
+                        # human_frames.append(frame[human_y:human_y2, human_x:human_x2])
+
+                frame_count += 1
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
+            dict[video_count]["skateboard"] = skateboard_frames
+            #dict[video_count]["human"] = human_frames
+            dict[video_count]["label"] = video_filename
+            video_count+=1
 if __name__ == "__main__":
     main()
